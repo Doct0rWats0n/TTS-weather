b662acc09f93a5aaf6cb683b45523fbbae018794
Doct0rWats0n <66429733+Doct0rWats0n@users.noreply.github.com>
2021-03-28 15:35:31 +0500

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -1,8 +1,10 @@
 from gtts import gTTS
 import requests
 
+API = ''
+
 responce = requests.get('http://api.openweathermap.org/data/2.5/weather?' +
-                        'q=London&units=metric&appid=APIключ')
+                        'q=London&units=metric&appid={}'.format(API))
 weather = responce.json()
 
 tts = gTTS(f"Погода: \n{weather['main']['temp']} градусов по цельсию\n" +
